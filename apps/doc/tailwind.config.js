c9db04d4a6fa22a33a6ff227c22f72806d590711
sial <rimsila.itc@gmail.com>
2022-06-24 03:07:03 +0700

    fix: css

--- apps/doc/tailwind.config.js
+++ apps/doc/tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    './docs/**/*.tsx',
-    './node_modules/ui/**/*.{js,ts,jsx,tsx}',
-    // './node_modules/ui/*/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./docs/**/*.tsx', './node_modules/ui/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
