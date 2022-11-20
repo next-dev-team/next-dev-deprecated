2354d5ff3fc08f2f917e32eeeb33973203544ef8
sial <rimsila.itc@gmail.com>
2022-11-21 00:41:21 +0700

    docs: feat new docs

--- .dumirc.ts
+++ .dumirc.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   ],
   // autoAlias: false,
   outputPath: 'dist',
+  publicPath: '/next-dev/',
   themeConfig: {
     name: 'Next Dev',
     logo: 'https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png',
