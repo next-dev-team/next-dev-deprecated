a24f33c96a40e5b0928fc781b0cb2e0a2f043124
sila <rimsila.itc@gmail.com>
2022-12-11 22:23:08 +0700

    fix: fixed css conflict with antd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+// const plugin = require('tailwindcss/plugin');
+
 module.exports = {
+  important: true,
   content: [
     './docs/**/*.tsx',
     './packages/ui/**/*.tsx',
     './packages/antd-ui/**/*.tsx',
   ],
-  plugins: [require('@tailwindcss/line-clamp')]
-}
+  plugins: [require('@tailwindcss/line-clamp')],
+  corePlugins: {
+    preflight: false,
+  },
+};
